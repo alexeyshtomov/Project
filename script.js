7bce262a015e71ddbf4d051dae34a578f8e029f2
Alexey Shtomov <alexeyshtomov2001@gmail.com>
2023-09-20 22:38:15 +0300

    коммит2

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Обработчик события для кнопки "Забронировать билеты"
-    const reserveButton = document.getElementById('reserveButton');
+    // ...
+  
+    
     if (reserveButton) {
       reserveButton.addEventListener('click', function () {
         const ticketQuantityInput = document.getElementById('ticketQuantity');
         const sessionSelect = document.getElementById('sessionSelect');
+        const rowSelect = document.getElementById('rowSelect');
+        const seatSelect = document.getElementById('seatSelect');
         const reservationStatus = document.getElementById('reservationStatus');
   
         const ticketQuantity = parseInt(ticketQuantityInput.value);
         const selectedSession = sessionSelect.options[sessionSelect.selectedIndex].text;
+        const selectedRow = rowSelect.options[rowSelect.selectedIndex].text;
+        const selectedSeat = seatSelect.options[seatSelect.selectedIndex].text;
   
-        //код для бронирования билетов 
+        
+  
+        // код для бронирования билетов
   
         // пример
-        const message = `Вы успешно забронировали ${ticketQuantity} билет(ов) на сеанс "${selectedSession}".`;
+        const message = `Вы успешно забронировали ${ticketQuantity} билет(ов) на сеанс "${selectedSession}" в ряде ${selectedRow}, место ${selectedSeat}.`;
         reservationStatus.textContent = message;
       });
+  
+      // Обновляем опции выбора ряда и места при изменении выбранной сессии
+      sessionSelect.addEventListener('change', updateSeatOptions);
     }
   
-    // Другие обработчики событий и код, который  нужен 
+    
+    function updateSeatOptions() {
+      
+  
+    // ...
   });
+  
