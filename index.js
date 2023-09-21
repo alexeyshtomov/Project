850c705d877f6361769473647facdef99617743b
Alexey Shtomov <alexeyshtomov2001@gmail.com>
2023-09-21 23:23:49 +0300

    вернулиндекс

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// Базовый URL API
 const apiUrl = 'https://jscp-diplom.netoserver.ru/';
 
+// Функция для отправки POST-запроса
 async function sendPostRequest(endpoint, data) {
   try {
     const response = await fetch(apiUrl + endpoint, {
@@ -14,8 +16,11 @@ async function sendPostRequest(endpoint, data) {
       throw new Error(`Ошибка HTTP: ${response.status}`);
     }
 
+    
     const jsonResponse = await response.json();
     
+    
+
     return jsonResponse;
   } catch (error) {
     console.error('Произошла ошибка:', error);
@@ -23,15 +28,14 @@ async function sendPostRequest(endpoint, data) {
 }
 
 const requestData = 'event=update';
-
-
 sendPostRequest('https://jscp-diplom.netoserver.ru/', requestData) 
   .then((data) => {
-    
+    // Обработка данных из ответа API
     const halls = data.halls.result; // Список залов
     const films = data.films.result; // Список фильмов
     const seances = data.seances.result; // Список сеансов
 
+    
     console.log('Список залов:', halls);
     console.log('Список фильмов:', films);
     console.log('Список сеансов:', seances);
